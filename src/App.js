4959e10317dc40cab5bbae13522a3dea9a37e026
pradeep <pradeep@securly.com>
2021-03-24 16:25:19 +0530

    Header component and menu links added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { ShopPage } from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 export const Hats = () => {
   return <h1>Hats page</h1>
@@ -10,6 +11,7 @@ export const Hats = () => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}>
         </Route>
